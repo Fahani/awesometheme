98f01782c5ee3b9c169cee187310c2cf8f952861
nico <fahani45@gmail.com>
2018-07-21 09:49:11 +0200

    - Loading posts into the index.php page. - Setting a static page for the home page and a page for the blog posts - Adding custom css to the body when loading home (blog post pages) or the front page

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
 <h1>This is my index</h1>
+<?php
+    if( have_posts() ): // Check if we have posts before showing them
+
+        while( have_posts() ):
+
+            the_post();
+?>
+            <h3><?php the_title(); ?></h3>
+            <small>Posted on: <?php the_time( 'F j, Y' ); ?> at <?php the_time( 'g:i a' ); ?>, in <?php the_category(); ?></small>
+            <p><?php the_content(); ?></p>
+            <hr>
+<?php
+        endwhile;
+
+    endif;
+?>
 <?php get_footer(); ?>
