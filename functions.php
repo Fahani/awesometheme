59f1fd5d9aa20b023ab0d569b74131e70414bfbc
Nico <fahani45@gmail.com>
2018-07-22 17:50:07 +0200

    Creating a new post type (portfolio) - Adding the section into the admin dashboard to be able to create, view, edit, etc a portfolio. For that we are adding a new function in functions.php

--- functions.php
+++ functions.php
@@ -75,3 +75,40 @@ function awesome_remove_version() {
 }
 
 add_filter('the_generator','awesome_remove_version');
+
+/* CUSTOM POST TYPE */
+
+function awesome_custom_post_type() {
+    $label = array(
+        'name' => 'Portfolio',
+        'singular_name' => 'Portfolio',
+        'add_new' => 'Add Item',
+        'all_items' => 'All Item',
+        'all_new_item' => 'Add Item',
+        'edit_item' => "Edit item",
+        'new_item' => "New item",
+        'view_item' => 'View Item',
+        'search_item' => 'Search Portfolio',
+        'not_found' => 'No items found',
+        'not_found_in_trash' => 'No items found in trash',
+        'parent_item_colon' => 'Parent Item'
+    );
+    $args = array(
+        'labels' => $label,
+        'public' => true,
+        'has_archive' => true,
+        'publicly_queryable' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions'),
+        'taxonomies' => array('category', 'post_tag'),
+        'menu_position' => 5,
+        'exclude_from_search' => false
+    );
+
+    register_post_type('portfolio', $args);
+}
+
+add_action('init','awesome_custom_post_type');
